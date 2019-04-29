16305f43eed5abf858faef569f79fb7aad28182b
sheila237 <marthesheila.fouemeteugnitedemg@esprit.tn>
2019-04-29 01:16:11 +0100

    ff

--- core/BackEnd/supprimerCategorie.php
+++
@@ -1,9 +0,0 @@
-<?PHP
-include "categories/core/categoriesC.php";
-$categoriesC=new CategoriesC();
-if (isset($_POST["id"])){
-	$categoriesC->supprimerCategorie($_POST["id"]);
-	header('Location: all-professors.php');
-}
-
-?>
--- core/BackEnd/supprimerProduit.php
+++
@@ -1,9 +0,0 @@
-<?PHP
-include "produits/core/produitsCore.php";
-$produitsC=new ProduitsCore();
-if (isset($_POST["IDP"])){
-	$produitsC->supprimerProduits($_POST["IDP"]);
-	header('Location: library-assets.php');
-}
-
-?>
